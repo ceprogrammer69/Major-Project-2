4226aa60c3195977af3563c3d0b6c5e290d00160
ceprogrammer69 <jon.d90@gmail.com>
2023-02-06 09:58:20 +0800

    added app.js and product.js

---
+++ assets/js/product.js
@@ -0,0 +1,57 @@
+const products = [
+  // CPU
+  {
+    id: 0,
+    name: "CPU1",
+    price: 16388.9,
+    instock: 100,
+    description:
+      "Intel Core i7-12700K Alder Lake 3.6GHz Twelve-Core LGA 1700 Boxed Processor",
+    imgSrc: "./assets/img/cpu1.jpg",
+  },
+  {
+    id: 1,
+    name: "CPU2",
+    price: 18149.45,
+    instock: 43,
+    description:
+      "Intel Core i7-13700K Raptor Lake 3.4GHz Sixteen-Core LGA 1700 Boxed Processor ",
+    imgSrc: "EDIT",
+  },
+  {
+    id: 2,
+    name: "EDIT",
+    price: 19.99,
+    instock: 10,
+    description:
+      "EDIT",
+    imgSrc: "EDIT",
+  },
+  {
+    id: 3,
+    name: "EDIT",
+    price: 25.99,
+    instock: 5,
+    description:
+      "EDIT",
+    imgSrc: "EDIT",
+  },
+  {
+    id: 4,
+    name: "EDIT",
+    price: 29.99,
+    instock: 4,
+    description:
+      "EDIT",
+    imgSrc: "EDIT",
+  },
+  {
+    id: 5,
+    name: "EDIT",
+    price: 39.99,
+    instock: 40,
+    description:
+      "EDIT",
+    imgSrc: "EDIT",
+  },
+];
