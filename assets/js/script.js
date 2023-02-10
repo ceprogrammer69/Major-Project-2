fe6b83a911a18c7e70a833de8b43cb7d4ebf5d26
cjgabilan1 <christianjaygabilan@gmail.com>
2023-02-10 20:26:57 +0800

    accidentally deleted the function

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,23 +18,14 @@ $('.owl-carousel').owlCarousel({
     }
 })  
 
-// var owl = $("#owl-carousel-1").owlCarousel({
-//     loop:true,
-//     margin:10,
-//     nav:true,
-//     autoplay:true,
-//     autoplayTimeout:1000,
-//     autoplayHoverPause:true,
-//     responsive:{
-//         0:{
-//             items:1
-//         },
-//         600:{
-//             items:3
-//         },
-//         1000:{
-//             items:3
-//         }
-//     }
-// })
+let basket = JSON.parse(localStorage.getItem("data")) || []; 
+
+// Calculation of cart quantity inside the Cart Icon
+
+let calculation =()=>{
+  let cartIcon = document.getElementById("cartAmount");
+  cartIcon.innerHTML = basket.map((x)=> x.item ).reduce((x,y)=>x+y,0) // Changing cartIcon Number
+  //console.log(basket.map((x)=> x.item ).reduce((x,y)=>x+y,0));
+};
+calculation(); // everytime the application loads, calculation() runs
   
